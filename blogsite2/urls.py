e85413e7afade0eb219575f136ff9768bad41ad4
jorgemedinabaez <jorgemedinabaez88@gmail.com>
2023-10-27 22:05:48 -0300

    Se termina de crear el CRUD utilizando Django en la app blogsite2

--- blogsite2/urls.py
+++ blogsite2/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index,add,addregister
+from .views import index,add,addregister,delete,update,updateregister
 
 urlpatterns = [
     path('', index, name='index'),
     path('add/', add, name='add'),
     path('add/addregister/', addregister, name='addregister'),
+    path('delete/<int:id>/', delete, name='delete'),
+    path('update/<int:id>/', update, name='update'),
+    path('updateregister/<int:id>/', updateregister, name='updateregister'),
+
 ]
