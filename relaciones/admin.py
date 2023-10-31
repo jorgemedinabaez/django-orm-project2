65ed3dc452cc499e5d31c91e7f259d47d0a65c25
jorgemedinabaez <jorgemedinabaez88@gmail.com>
2023-10-31 00:37:22 -0300

    Se realizan ejemplos de relaciones 'one-to-one','one-to-many' y 'many-to-many', vía CRUD.

--- relaciones/admin.py
+++ relaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ModeloAuto,Fabricante,TipoCombustible,DirectorEjecutivo
 
 # Register your models here.
+
+admin.site.register(ModeloAuto)
+admin.site.register(Fabricante)
+admin.site.register(TipoCombustible)
+admin.site.register(DirectorEjecutivo)
